6dfa4d58709662b9a97e69a5b94c7f7134bbe7e0
Iskusnik <iskusnikxd@gmail.com>
2019-09-26 04:02:53 +0500

    Add files via upload

---
+++ precache-manifest.03b47cf621e4cb933383857df14aa1b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27d159049db6230a071a5e0f87d5235",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ed5fd85569ab2d4907",
+    "url": "/static/css/main.86e1eee6.chunk.css"
+  },
+  {
+    "revision": "e9dcc1cb8f63385e2d5d",
+    "url": "/static/js/2.90ddbf7b.chunk.js"
+  },
+  {
+    "revision": "91ed5fd85569ab2d4907",
+    "url": "/static/js/main.83dc15bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06f804f0881971ee3494fdf3ff113de4",
+    "url": "/static/media/rocket.06f804f0.png"
+  },
+  {
+    "revision": "46df961c19d4fb636d8b19e6f9259cfb",
+    "url": "/static/media/storage.46df961c.png"
+  }
+]);
